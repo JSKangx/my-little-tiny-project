d6d41badbd34a3684178c3c5339f2ea41ef7a127
Jinsu <injnamek@gmail.com>
2024-08-31 10:47:52 +0900

    figma에서 추출한 색상 커스텀 유틸리티 등록

--- 01-purchasingDonuts/tailwind.config.js
+++ 01-purchasingDonuts/tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
       boxShadow: {
         "shadow-my": "inset 0 0 10px red",
       },
+      colors: {
+        primary: {
+          100: "#FED8DF",
+          200: "#FF9494",
+          300: "#FF7074",
+        },
+        secondary: {
+          100: "#585858",
+          200: "#000000",
+        },
+      },
+      borderRadius: {
+        none: 0,
+        "": 50,
+      },
     },
   },
   plugins: [],
